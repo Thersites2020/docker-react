989f24f6a62a103776058bc09c2da69dfb8920d0
Thersites2020 <nic.thorne@tutanota.com>
2022-07-31 19:12:09 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Text changed again!
+          Feature-branch-changed-text
         </p>
         <a
           className="App-link"
